5d5f07a375a3e0f8988024a69f4dba463defc6cc
Ogun Isik <ogunisik@gmail.com>
2019-06-05 22:26:59 +0300

    python black format duzenlemeleri

--- src/main.py
+++ src/main.py
@@ -10,42 +10,45 @@ CLIENT = MongoClient("localhost", 27017)
 MONGO_TOPICS = CLIENT.gts.madde
 GEVENT_JOBS = []
 
+
 def get_api_result(word):
-  """ GTS uzerinden kelimeyi getirir """
+    """ GTS uzerinden kelimeyi getirir """
 
-  headers = {
-      "User-Agent": "APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)"
-  }
+    headers = {
+        "User-Agent": "APIs-Google (+https://developers.google.com/webmasters/APIs-Google.html)"
+    }
 
-  api_url = f"http://www.sozluk.gov.tr/gts?ara={word}"
-  result = requests.get(api_url, headers=headers)
+    api_url = f"http://www.sozluk.gov.tr/gts?ara={word}"
+    result = requests.get(api_url, headers=headers)
 
-  if result.status_code != 200:
-    print("API error. Status Code: {result.status_code}. Word: {word}")
-    return None
+    if result.status_code != 200:
+        print("API error. Status Code: {result.status_code}. Word: {word}")
+        return None
 
-  return result.text
+    return result.text
+
 
 def get_results():
-  """ Elimizdeki kelime listesi uzerinden kelimeleri getirir """
-  for word in consts.WORDS:
-    response = get_api_result(word)
-    if not response:
-      continue
+    """ Elimizdeki kelime listesi uzerinden kelimeleri getirir """
+    for word in consts.WORDS:
+        response = get_api_result(word)
+        if not response:
+            continue
 
-    maddeler = json.loads(response)
+        maddeler = json.loads(response)
 
-    for madde in maddeler:
-      if not isinstance(madde, dict):
-        print(f"Wrong type for: {word}. Detail: {madde}")
-        continue
+        for madde in maddeler:
+            if not isinstance(madde, dict):
+                print(f"Wrong type for: {word}. Detail: {madde}")
+                continue
 
-      madde_id = madde["madde_id"]
-      
-      mongo_obj = madde.copy()
-      mongo_obj["_id"] = madde_id
+            madde_id = madde["madde_id"]
 
-      MONGO_TOPICS.replace_one({"_id": madde_id}, mongo_obj, upsert=True)
+            mongo_obj = madde.copy()
+            mongo_obj["_id"] = madde_id
+
+            MONGO_TOPICS.replace_one({"_id": madde_id}, mongo_obj, upsert=True)
+
 
 if __name__ == "__main__":
-  get_results()
+    get_results()
