33a0a561494fb46558e48f9d2e01d1872f1760ab
Kemal Ogun Isik <ogun.isik@deliveryhero.com>
2024-03-29 23:18:35 +0100

    SQLite kolonlari isme gore siralandi

--- sozluk/v12/src/guncel-turkce-sozluk.db.sql
+++ sozluk/v12/src/guncel-turkce-sozluk.db.sql
@@ -2,9 +2,9 @@ BEGIN TRANSACTION;
 CREATE TABLE IF NOT EXISTS "ornek" (
 	"ornek_id"	INTEGER NOT NULL,
 	"anlam_id"	INTEGER NOT NULL,
+	"kac"	INTEGER NOT NULL,
 	"ornek_sira"	INTEGER NOT NULL,
 	"ornek"	TEXT,
-	"kac"	INTEGER NOT NULL,
 	"yazar_id"	INTEGER,
 	"yazar_vd"	TEXT,
 	PRIMARY KEY("ornek_id"),
@@ -13,12 +13,12 @@ CREATE TABLE IF NOT EXISTS "ornek" (
 );
 CREATE TABLE IF NOT EXISTS "anlam" (
 	"anlam_id"	INTEGER NOT NULL,
-	"madde_id"	INTEGER NOT NULL,
 	"anlam_sira"	INTEGER NOT NULL,
-	"fiil"	INTEGER NOT NULL,
-	"tipkes"	INTEGER NOT NULL,
 	"anlam"	TEXT NOT NULL,
+	"fiil"	INTEGER NOT NULL,
 	"gos"	INTEGER NOT NULL,
+	"madde_id"	INTEGER NOT NULL,
+	"tipkes"	INTEGER NOT NULL,
 	PRIMARY KEY("anlam_id"),
 	FOREIGN KEY("madde_id") REFERENCES "madde"("madde_id")
 );
@@ -37,40 +37,40 @@ CREATE TABLE IF NOT EXISTS "atasozu" (
 );
 CREATE TABLE IF NOT EXISTS "madde" (
 	"madde_id"	INTEGER NOT NULL,
+	"anlam_gor"	INTEGER NOT NULL,
+	"anlam_say"	INTEGER NOT NULL,
+	"birlesikler"	TEXT,
+	"cesit_say"	INTEGER NOT NULL,
+	"cesit"	INTEGER NOT NULL,
+	"cogul_mu"	INTEGER NOT NULL,
+	"egik_mi"	INTEGER NOT NULL,
+	"font"	TEXT,
+	"gosterim_tarihi"	TEXT,
 	"kac"	INTEGER NOT NULL,
 	"kelime_no"	INTEGER NOT NULL,
-	"cesit"	INTEGER NOT NULL,
-	"anlam_gor"	INTEGER NOT NULL,
-	"on_taki"	TEXT,
-	"madde"	TEXT NOT NULL,
-	"cesit_say"	INTEGER NOT NULL,
-	"anlam_say"	INTEGER NOT NULL,
-	"taki"	TEXT,
-	"cogul_mu"	INTEGER NOT NULL,
-	"ozel_mi"	INTEGER NOT NULL,
 	"lisan_kodu"	INTEGER NOT NULL,
 	"lisan"	TEXT,
-	"telaffuz"	TEXT,
-	"birlesikler"	TEXT,
-	"font"	TEXT,
 	"madde_duz"	TEXT NOT NULL,
-	"gosterim_tarihi"	TEXT,
-	"egik_mi"	INTEGER NOT NULL,
+	"madde"	TEXT NOT NULL,
+	"on_taki"	TEXT,
+	"ozel_mi"	INTEGER NOT NULL,
+	"taki"	TEXT,
+	"telaffuz"	TEXT,
 	PRIMARY KEY("madde_id")
 );
 CREATE TABLE IF NOT EXISTS "ozellik" (
 	"ozellik_id"	INTEGER NOT NULL,
-	"tur"	INTEGER NOT NULL,
-	"tam_adi"	TEXT NOT NULL,
-	"kisa_adi"	TEXT NOT NULL,
 	"ekno"	INTEGER NOT NULL,
+	"kisa_adi"	TEXT NOT NULL,
+	"tam_adi"	TEXT NOT NULL,
+	"tur"	INTEGER NOT NULL,
 	PRIMARY KEY("ozellik_id")
 );
 CREATE TABLE IF NOT EXISTS "yazar" (
 	"yazar_id"	INTEGER NOT NULL,
-	"tam_adi"	TEXT NOT NULL,
-	"kisa_adi"	TEXT NOT NULL,
 	"ekno"	INTEGER NOT NULL,
+	"kisa_adi"	TEXT NOT NULL,
+	"tam_adi"	TEXT NOT NULL,
 	PRIMARY KEY("yazar_id")
 );
 CREATE TABLE IF NOT EXISTS "madde_atasozu" (
